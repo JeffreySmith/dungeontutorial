b672bccc23cad6a03577f49c2f8b97593ade6830
Jeffrey Smith <jeffrey.smith7@gmail.com>
2023-01-06 17:18:01 -0500

    Initial turn system

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod camera;
 mod components;
 mod spawner;
 mod systems;
+mod turn_state;
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH:i32=80;
@@ -16,6 +17,7 @@ mod prelude {
     pub use crate::components::*;
     pub use crate::spawner::*;
     pub use crate::systems::*;
+    pub use crate::turn_state::*;
     pub use legion::*;
     pub use legion::world::SubWorld;
     pub use legion::systems::CommandBuffer;
@@ -29,7 +31,10 @@ use prelude::*;
 struct State {
     ecs: World,
     resources:Resources,
-    systems:Schedule,
+   
+    input_systems:Schedule,
+    player_systems:Schedule,
+    monster_systems:Schedule,
 }
 impl State {
     fn new() -> Self {
@@ -48,10 +53,14 @@ impl State {
         
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
+        resources.insert(TurnState::AwaitingInput);
         Self{
             ecs,
             resources,
-            systems:build_scheduler()
+            input_systems:build_input_scheduler(),
+            player_systems:build_player_scheduler(),
+            monster_systems:build_monster_scheduler(),
+                
         }
     }
 }
@@ -62,7 +71,22 @@ impl GameState for State {
         ctx.set_active_console(1);
         ctx.cls();
         self.resources.insert(ctx.key);
-        self.systems.execute(&mut self.ecs,&mut self.resources);
+
+        let current_state = self.resources.get::<TurnState>().unwrap().clone();
+        match current_state {
+            TurnState::AwaitingInput=>self.input_systems.execute(
+                &mut self.ecs,
+                &mut self.resources
+            ),
+            TurnState::PlayerTurn => {
+                self.player_systems.execute(&mut self.ecs,&mut self.resources);
+            }
+            TurnState::MonsterTurn=> {
+                self.monster_systems.execute(&mut self.ecs,&mut self.resources)
+            }
+        }
+
+        
         render_draw_buffer(ctx).expect("Render error");
 
     }
--- src/spawner.rs
+++ src/spawner.rs
@@ -28,7 +28,8 @@ pub fn spawn_enemy(
                  2=>to_cp437('o'),
                  _=>to_cp437('g'),
              }
-         }
+         },
+         MovingRandomly{}
         )
     );
 }
