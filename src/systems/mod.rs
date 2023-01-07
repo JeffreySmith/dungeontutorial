9ea900c6b1c29787c4137bab32e9ad087c2303f7
Jeffrey Smith <jeffrey.smith7@gmail.com>
2023-01-07 13:30:12 -0500

    Tooltips, plus bug in main.rs fixed (SCREEN vs DISPLAY)

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -7,6 +7,7 @@ mod collisions;
 mod random_move;
 mod end_turn;
 mod movement;
+mod tooltips;
   mod hud;
   pub fn build_input_scheduler() -> Schedule {
       Schedule::builder()
@@ -15,6 +16,7 @@ mod movement;
           .add_system(map_render::map_render_system())
           .add_system(entity_render::entity_render_system())
           .add_system(hud::hud_system())
+          .add_system(tooltips::tooltips_system())
           .build()
 }
 pub fn build_player_scheduler() -> Schedule {
@@ -40,6 +42,7 @@ pub fn build_monster_scheduler() -> Schedule {
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .add_system(hud::hud_system())
+        
         .add_system(end_turn::end_turn_system())
         .build()
 }
--- src/turn_state.rs
+++ src/turn_state.rs
@@ -5,3 +5,4 @@ pub enum TurnState {
     PlayerTurn,
     MonsterTurn,
 }
+
